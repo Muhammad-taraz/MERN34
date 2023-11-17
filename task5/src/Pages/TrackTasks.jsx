60690d64e1574745ede624b513f630d2361c182c
suleman15 <sulemanahmed2704@gmail.com>
2023-11-17 16:06:37 +0500

    task add and delete

--- task5/src/Pages/TrackTasks.jsx
+++ task5/src/Pages/TrackTasks.jsx
@@ -2,44 +2,52 @@ import React, { useState } from 'react'
 import Header from '../Header'
 
 export default function TrackTasks() {
-    const [todos, setTodo] = useState([]);
+    const [todos, setTodo] = useState([]);  
     const [task, setTask] = useState("");
-    const [status, setStatus] = useState("completed");
+    const [status, setStatus] = useState("Add");
 
     return (
         <>
             <Header />
-            <div className='text-5xl font-bold'>TrackTasks</div>
+            <div className='text-5xl font-bold'>TrackTasks
             <form onSubmit={(e) => {
                 e.preventDefault();
-                setTodo([...todos,
-                {
-                    task: task,
-                    status: status
-                }
-            ]);}}>
-                <label className='p-3 m-3'>Task</label>
-                <input placeholder='Add task' required type='text' value={task}
+                setTodo([...todos,{task:"task"}])
+                }}>
+                <label className='p-3 mt-5'>Task</label>
+                <input className='mt-5' placeholder='Add task' required type='text' value={task}
                 onChange={(e) => {
                   setTask(e.target.value);}}/>
-                  <select  required value={status}
-                  onChange={(e) =>{
-                    setStatus(e.target.value);
-                  }}>
-                    <button type="Add items">Add items</button>
-                    <button type="Delete items">Delete items</button>
-
-                  </select>
-               
+                 
+                  
+                  
+                  <div className='flex gap-5 mt-5'>
+                   <button type='Add' className='bg-slate-500 rounded-md hover:bg-slate-950 border-black'>Add items</button>
+                   
+                    </div>
 
             </form>
+            <div>
+                {todos.map ((item,index) => {
+                    return (
+                        <div key={index} > 
+                            <span>{item.task}</span>
+                            <span>{item.status}</span>
+                            <button type='delete' className='bg-slate-500 rounded-md hover:bg-slate-950 border-black p-3 gap-5 mt-5' onClick={()=>{
 
+                                const remove = todos.filter((todoItem, i) => {
+                                    return i != index;
+                                })
+                                setTodo (remove);
+                            }}>Delete</button>
+                            </div>
+                    );
+                  
+                })}
 
-
-
-
-
-
+                
+            </div>
+           </div>
         </>
     )
 }

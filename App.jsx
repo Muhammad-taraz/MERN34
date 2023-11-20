7066b07304972c641cd1ee0a2dd362e749940058
Muhammad-taraz <148982614+Muhammad-taraz@users.noreply.github.com>
2023-11-20 13:57:24 +0500

    Add files via upload
    Calculator

--- App.jsx
+++ App.jsx
@@ -2,42 +2,66 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Sawal from 'sweetalert2'
 
-
-function App() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [message, setMessage] = useState("")
-  const Submit = (e) => {
-    e.preventDefault();
-    Sawal.fire(
-      "form submitted",
-      `Name:${name}, Email:${email}, Username:${username}, Password:${password}, Message:${message}`
-    )
-  }
-
+function Caculator () {
+   const [firstValue, setFirstValue] = useState(0);
+   const [secondValue, setSecondValue] = useState(0);
+   const [result, setResult] = useState(0);
+   switch (value) {
+    case 1 :
+      firstValue + secondValue;
+      break;
+      case 2 :
+        firstValue - secondValue;
+        break;
+        case 3 :
+          firstValue * secondValue;
+          break;
+          case 4 :
+            firstValue / secondValue;
+            break;
+            case 5 :
+              firstValue % secondValue;
+             
+   }
   return (
     <>
-    <div>
-      <h1 className='text-5xl font-black'>Contact Form</h1>
-    </div><br/><br/><br/><br/><br/>
-    
+    <form action='' className='flex justify-center item-center flex flex-col gap-3' onSubmit={(e) => {
+      e.preventDefault
+    }}>
+      <input type='number' value={firstValue} onChange={(e) => {
+        setFirstValue(Number(e.target.value))}} placeholder='Enter first no.' className='border-gray-500 rounded-md p-2'/>
+        <input type='number' value={secondValue} onChange={(e) => {
+        setSecondValue(Number(e.target.value))}} placeholder='Enter second no.' className='border-gray-500 rounded-md p-2'/>
       <div>
-        <form onSubmit={Submit}>
-          <input type='text' placeholder='Enter your name' required onChange={(e)=>{setName(e.target.value)}} className='bg-grey-192 gap-2.5 text-lg flex-col justify-center border-2'/><br/><br/>
-          <input type='email'  placeholder='Enter your email' required onChange={(e)=>{setEmail(e.target.value)}} className='bg-grey-192 gap-2.5 text-lg flex-col justify-center border-2' /><br/><br/>
-          <input type='username'  placeholder='Enter your username' required onChange={(e)=>{setUsername(e.target.value)}} className='bg-grey-192 gap-2.5 text-lg flex-col justify-center border-2'/><br/><br/>
-          <input type='password'  placeholder='Enter your password' required onChange={(e)=>{setPassword(e.target.value)}} className='bg-grey-192 gap-2.5 text-lg flex-col justify-center border-2'/><br/><br/>
-          <input type='message' required onChange={(e)=>{setMessage(e.target.value)}} className='text-7xl border-2 border-black ' />
-          <br/><br/><br/><br/>
-          <button className='bg-green-200 border-2 text-2xl'>Submit</button>
+        <h1 className='text-7xl font-bold'>Simple Calulator</h1>
+        </div>
+        <div className='gap-5 justify-center flex '>
+          <button type='submit' className='bg-green-500 rounded-md' onClick={() => {
+            setResult(firstValue + secondValue)
+          }}>+</button>
+         <button type='submit' className='bg-green-500 rounded-md' onClick={() => {
+            setResult(firstValue - secondValue)
+          }}>+</button>
+          <button type='submit' className='bg-green-500 rounded-md' onClick={() => {
+            setResult(firstValue * secondValue)
+          }}>+</button>
+          <button type='submit' className='bg-green-500 rounded-md' onClick={() => {
+            setResult(firstValue / secondValue)
+          }}>+</button>
+          <button type='submit' className='bg-green-500 rounded-md' onClick={() => {
+            setResult(firstValue % secondValue)
+          }}>+</button>
+      
+      
+
+    
+    
+    
+        </div>
         </form>
-      </div>
     </>
   )
 }
 
-export default App
+export default Caculator

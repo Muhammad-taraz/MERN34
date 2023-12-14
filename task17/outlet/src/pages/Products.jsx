d12239493052f5f97f2b5a9485d5f959a985d564
Muhammad-taraz <muhammadtarazansari@gmail.com>
2023-12-14 15:11:23 +0500

    outlet

--- task17/outlet/src/pages/Products.jsx
+++ task17/outlet/src/pages/Products.jsx
@@ -1,5 +1,41 @@
 import React from "react";
-
+import ProductData from '../Products/ProductData.json'
 export default function Products() {
-  return <div>Products</div>;
+  //console.log("ProductData")
+     
+  return <div className="flex font-semibold flex-col italic ml-12">
+
+         
+
+          {ProductData.map((Products, i)  => {
+
+            return ( <>
+            <div key={i} className="grid mt-5 ">
+
+               <img className="w-[25vh] flex "
+               src={`https://api.dicebear.com/7.x/icons/svg?seed=Molly = ${Products.name}`}
+               alt="avatar" />
+            <div>{Products.name}</div>
+            <div>{Products.category}</div>
+            <div>{Products.availability}</div>
+            <div>{Products.price}</div>
+            <div>{Products.description}</div>
+              </div>
+                  </> 
+          
+                  )
+          }
+          
+          
+          
+          )
+          
+          
+          
+          }
+
+
+         </div>
+  
+  
 }
